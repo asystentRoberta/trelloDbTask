c616eee28e9a80c978b76f47ca530a355da1bd2d
Asystent <gitkrakent@asystent-24.pl>
2019-12-07 20:33:34 +0100

    First ("core") class and DTO for this class

---
+++ src/main/java/pl/com/bohdziewicz/trelloDbTask/domain/Task.java
@@ -0,0 +1,8 @@
+package pl.com.bohdziewicz.trelloDbTask.domain;
+
+public class Task {
+
+    private Long id;
+    private String title;
+    private String content;
+}
---
+++ src/main/java/pl/com/bohdziewicz/trelloDbTask/domain/TaskDTO.java
@@ -0,0 +1,8 @@
+package pl.com.bohdziewicz.trelloDbTask.domain;
+
+public class TaskDTO {
+
+    private Long id;
+    private String title;
+    private String content;
+}
