e360a33a351eee6c50179764cf47dd2ab8b18964
AsystentRoberta <asystent.roberta@gmail.com>
2020-04-26 21:16:16 +0200

    Prepare to sending nicer email ;) * created-trello-card-mail.html => template email for thymeleaf (wrote by front-end guy) - it will proceed in next commits * MailCreatorService => using bean provided by Thymeleaf (templateEngine) - it will proceed in next commits * SimpleEmailService -> use MailCreatorService to prepare MimeMail (before send): MimeMessagePreparator -> MailCreatorService.buildTrelloCardEmail * notepad.txt -> add additional task

--- src/main/java/pl/com/bohdziewicz/trelloDbTask/service/SimpleEmailService.java
+++ src/main/java/pl/com/bohdziewicz/trelloDbTask/service/SimpleEmailService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import pl.com.bohdziewicz.trelloDbTask.config.MailConfig;
@@ -13,14 +15,17 @@ import pl.com.bohdziewicz.trelloDbTask.domain.Mail;
 @Service
 public class SimpleEmailService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
     private final JavaMailSenderImpl javaMailSender;
     private final MailConfig mailConfig;
-    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+    private final MailCreatorService mailCreatorService;
 
-    public SimpleEmailService(JavaMailSenderImpl javaMailSender, MailConfig mailConfig) {
+    public SimpleEmailService(JavaMailSenderImpl javaMailSender, MailConfig mailConfig,
+            MailCreatorService mailCreatorService) {
 
         this.mailConfig = mailConfig;
         this.javaMailSender = javaMailSender;
+        this.mailCreatorService = mailCreatorService;
         javaMailSender.setUsername(mailConfig.getMailSender());
         javaMailSender.setPassword(mailConfig.getMailPassword());
     }
@@ -30,19 +35,29 @@ public class SimpleEmailService {
         LOGGER.info("Starting email preparation...");
 
         try {
-            javaMailSender.send(createMailMessage(mail));
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been sent.");
         } catch (MailException exception) {
             LOGGER.error("Failed to process email sending: " + exception.getMessage(), exception);
         }
     }
 
-    private SimpleMailMessage createMailMessage(final Mail mail) {
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private SimpleMailMessage createSimpleMailMessageWithoutTemplates(final Mail mail) {
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
         return mailMessage;
     }
 }
