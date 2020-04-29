853daaabcdccf6b37b66e34ac03e9d506e795cde
AsystentRoberta <asystent.roberta@gmail.com>
2020-04-29 19:49:34 +0200

    Using list in thymleaf template

--- src/main/java/pl/com/bohdziewicz/trelloDbTask/service/MailCreatorService.java
+++ src/main/java/pl/com/bohdziewicz/trelloDbTask/service/MailCreatorService.java
@@ -1,5 +1,8 @@
 package pl.com.bohdziewicz.trelloDbTask.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -19,6 +22,10 @@ public class MailCreatorService {
 
     public String buildTrelloCardEmail(String message) {
 
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can menage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("App allows sending tasks to trllo");
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://bohdziewicz.com.pl/java_tests/index.html");
@@ -27,6 +34,8 @@ public class MailCreatorService {
         context.setVariable("companyDetail", "Trello_DB_APP by bohdziewicz.com.pl");
         context.setVariable("show_button", true);
         context.setVariable("is_friend", false);
+        context.setVariable("app_functions", functionality);
+
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
